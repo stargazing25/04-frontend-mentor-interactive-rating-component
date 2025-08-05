4045ecaad11e01d0d036ec6963e204109201374b
huderdcv <huder.delacruz.27@unsch.edu.pe>
2025-08-04 22:50:37 -0500

    feat: clean the form when we back

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,17 @@
+// variables for form
 const $form = document.getElementById('rating-options-form');
 const $ratingContainer = document.querySelector('.rating-container');
 const $responseContainer = document.querySelector('.response-container');
 const $responseCount = document.querySelector('.response-count');
+let $radioChecked = null;
 
+// variables for button back
 const $backButton = document.querySelector('.back-button');
 
+// form
 $form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const $radioChecked = document.querySelector('input[type="radio"]:checked');
-  console.log($radioChecked);
+  $radioChecked = document.querySelector('input[type="radio"]:checked');
   if (!$radioChecked) return alert('Please select at least one option!');
 
   const rating = $radioChecked.value;
@@ -19,7 +22,9 @@ $form.addEventListener('submit', (e) => {
   $responseContainer.style.display = 'flex';
 });
 
+// button
 $backButton.addEventListener('click', () => {
+  $radioChecked.checked = false;
   $backButton.style.display = 'none';
   $responseContainer.style.display = 'none';
   $ratingContainer.style.display = 'flex';
